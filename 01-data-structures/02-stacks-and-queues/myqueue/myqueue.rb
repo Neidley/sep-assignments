c1181a7838e58b8c206507cf7165d7edb5179025
Andrew Neidley <neidz44@gmail.com>
2017-12-08 18:42:57 -0600

    Checkpoint Complete

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,24 @@ class MyQueue
   end
 
   def enqueue(element)
+    @queue[@queue.length] = element
+    @tail = element
+    @head = @queue.first
   end
 
   def dequeue
+    temp = @queue.first
+    @queue.delete_at(0)
+    @tail = @queue.last
+    @head = @queue.first
+    return temp
   end
 
   def empty?
+    if @queue.length == 0
+      true
+    else
+      false
+    end
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,23 @@ class MyStack
   end
 
   def push(item)
+    @stack[@stack.length] = item
+    self.top = item
   end
 
   def pop
+    temp = @stack.last
+    @stack.delete_at(@stack.length-1)
+    self.top = @stack.last
+    return temp
   end
 
   def empty?
+    if @stack.length == 0
+      true
+    else
+      false
+    end
   end
+
 end
