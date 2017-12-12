32b21393521be4224a636c0c2fd187276a4de0b7
Andrew Neidley <neidz44@gmail.com>
2017-12-12 11:05:38 -0600

    three tests failing

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -5,23 +5,26 @@ class HashClass
   end
 
   def []=(key, value)
+    @items[index(key,size)] = value
   end
 
-
   def [](key)
+    @items[index(key,size)]
   end
 
   def resize
+    @items = Array.new(size*2)
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.count
   end
-
 end
