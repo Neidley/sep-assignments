ba2c1c63f8fd5e93cd88c89f6d4b3f2b7b4f8df8
Andrew Neidley <neidz44@gmail.com>
2017-12-15 01:27:49 -0600

    still in progress

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -5,15 +5,36 @@ class HashClass
   end
 
   def []=(key, value)
-    @items[index(key,size)] = value
+    @hash = HashItem.new(key,value)
+    @items.each do |item|
+      if item == @hash
+        return item
+      elsif item == nil
+        @items[index(key,size)] = @hash
+      else
+        resize
+      end
+    end
+    p @items
+    p @items[index(key,size)].value
+    p @items.size
   end
 
   def [](key)
-    @items[index(key,size)]
+    @items[index(key,size)].value
   end
 
   def resize
-    @items = Array.new(size*2)
+    old = @items
+    @items = Array.new(size * 2)
+    old.each do |item|
+      if !item
+        next
+      else
+        @items[index(item.key,size)] = item
+      end
+    end
+    @items
   end
 
   # Returns a unique, deterministically reproducible index into an array
